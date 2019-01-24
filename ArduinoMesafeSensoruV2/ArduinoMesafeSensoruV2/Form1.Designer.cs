40875077babe568435f183dc9b2ed2bd575f73bc
KursadArslan <39864768+KursadArslan@users.noreply.github.com>
2019-01-24 03:05:16 +0300

    Add files via upload

---
+++ ArduinoMesafeSensoruV2/ArduinoMesafeSensoruV2/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace ArduinoMesafeSensoruV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbBoxPort = new System.Windows.Forms.ComboBox();
+            this.cmbBoxSerial = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.btnBaglan = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.picBaglandi = new System.Windows.Forms.PictureBox();
+            this.picKesildi = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.picBaglandi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picKesildi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(10, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Port Seçimi:";
+            // 
+            // cmbBoxPort
+            // 
+            this.cmbBoxPort.FormattingEnabled = true;
+            this.cmbBoxPort.Location = new System.Drawing.Point(13, 26);
+            this.cmbBoxPort.Name = "cmbBoxPort";
+            this.cmbBoxPort.Size = new System.Drawing.Size(133, 21);
+            this.cmbBoxPort.TabIndex = 4;
+            // 
+            // cmbBoxSerial
+            // 
+            this.cmbBoxSerial.FormattingEnabled = true;
+            this.cmbBoxSerial.Location = new System.Drawing.Point(173, 26);
+            this.cmbBoxSerial.Name = "cmbBoxSerial";
+            this.cmbBoxSerial.Size = new System.Drawing.Size(133, 21);
+            this.cmbBoxSerial.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(170, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Serial Seçimi:";
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDurdur.Location = new System.Drawing.Point(451, 20);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(128, 27);
+            this.btnDurdur.TabIndex = 10;
+            this.btnDurdur.Text = "Bağlantıyı Kes";
+            this.btnDurdur.UseVisualStyleBackColor = true;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // btnBaglan
+            // 
+            this.btnBaglan.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBaglan.Location = new System.Drawing.Point(317, 20);
+            this.btnBaglan.Name = "btnBaglan";
+            this.btnBaglan.Size = new System.Drawing.Size(128, 27);
+            this.btnBaglan.TabIndex = 9;
+            this.btnBaglan.Text = "Cihaza Bağlan";
+            this.btnBaglan.UseVisualStyleBackColor = true;
+            this.btnBaglan.Click += new System.EventHandler(this.btnBaglan_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(612, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Bağlantı Durumu:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(83, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "label6";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(12, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Mesafe =";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(253, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(154, 17);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Cisim Yakınlık Durumu:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(411, 78);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(416, 23);
+            this.progressBar1.TabIndex = 18;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // picBaglandi
+            // 
+            this.picBaglandi.Image = ((System.Drawing.Image)(resources.GetObject("picBaglandi.Image")));
+            this.picBaglandi.Location = new System.Drawing.Point(727, 23);
+            this.picBaglandi.Name = "picBaglandi";
+            this.picBaglandi.Size = new System.Drawing.Size(32, 32);
+            this.picBaglandi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picBaglandi.TabIndex = 20;
+            this.picBaglandi.TabStop = false;
+            // 
+            // picKesildi
+            // 
+            this.picKesildi.Image = ((System.Drawing.Image)(resources.GetObject("picKesildi.Image")));
+            this.picKesildi.Location = new System.Drawing.Point(727, 23);
+            this.picKesildi.Name = "picKesildi";
+            this.picKesildi.Size = new System.Drawing.Size(32, 32);
+            this.picKesildi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picKesildi.TabIndex = 21;
+            this.picKesildi.TabStop = false;
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 110);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            series1.Legend = "Legend1";
+            series1.Name = "Veri";
+            series1.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(815, 272);
+            this.chart1.TabIndex = 22;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 394);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.picKesildi);
+            this.Controls.Add(this.picBaglandi);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDurdur);
+            this.Controls.Add(this.btnBaglan);
+            this.Controls.Add(this.cmbBoxSerial);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbBoxPort);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Anlık Mesafe Kontrolü V2 / Kürşad Arslan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBaglandi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picKesildi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbBoxPort;
+        private System.Windows.Forms.ComboBox cmbBoxSerial;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.Button btnBaglan;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox picBaglandi;
+        private System.Windows.Forms.PictureBox picKesildi;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ ArduinoMesafeSensoruV2/ArduinoMesafeSensoruV2/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Windows.Forms.DataVisualization.Charting;
+namespace ArduinoMesafeSensoruV2
+{
+    public partial class Form1 : Form
+    {
+        string[] portlar = SerialPort.GetPortNames();
+        DateTime yeni = DateTime.Now;
+        int zaman = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            label6.Visible = false;
+            label6.Text = "";
+            picBaglandi.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach(string port in portlar)
+            {
+                cmbBoxPort.Items.Add(port);
+                cmbBoxPort.SelectedIndex = 0;
+            }
+            cmbBoxSerial.Items.Add("4800");
+            cmbBoxSerial.Items.Add("9600");
+            cmbBoxSerial.SelectedIndex = 1;
+            picKesildi.Visible = true;
+            //label4.Text = "Bağlantı Kapalı";
+            this.chart1.Titles.Add("Ölçülen Veriler");
+            
+        }
+
+        private void btnBaglan_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            label6.Visible = true;
+            if (serialPort1.IsOpen == false)
+            {
+                if (cmbBoxPort.Text == "")
+                    return;
+                serialPort1.PortName = cmbBoxPort.Text;
+                serialPort1.BaudRate = Convert.ToInt16(cmbBoxSerial.Text);
+                try
+                {
+                    serialPort1.Open();
+                    picBaglandi.Visible = true;
+                    picKesildi.Visible = false;
+                    //label4.Text = "Bağlantı Açık";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata: " + ex.Message);
+                    throw;
+                }
+            }
+            else
+                picBaglandi.Visible = true;
+                picKesildi.Visible = false;
+                //label4.Text = "Bağlantı Kuruldu";
+        }
+
+        private void btnDurdur_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.Close();
+                picBaglandi.Visible = false;
+                picKesildi.Visible = true;
+                //label4.Text = "Bağlantı Kapalı";
+            }
+            progressBar1.Value = 0;
+            label6.Text = "Baglanti Kesildi";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {   // string data=serialPort1.ReadExisting();
+                double data = Convert.ToDouble(serialPort1.ReadLine());
+                label6.Text = data.ToString() + "" + "santimetre";
+                //  int data2 = Convert.ToInt16(serialPort1.ReadExisting());
+                double prog = (data * 100) / 200;
+                // data2 = ((data2 * 5000) / 1023) / 10;
+                progressBar1.Value = 100 - Convert.ToInt16(prog);
+                // serialPort1.DiscardInBuffer();//bunu silip farkı gor
+                //System.Threading.Thread.Sleep(100);
+                this.chart1.Series["Veri"].Points.AddXY(zaman, data);
+                zaman += 1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                timer1.Stop();
+
+            }
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+                serialPort1.Close();
+        }
+    }
+}
